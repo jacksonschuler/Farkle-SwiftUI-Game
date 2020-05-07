720d03b9e150a757b18e96a16037647bceac3e9b
BenBaker <bakerbenjamyn@gmail.com>
2020-05-07 14:21:44 -0600

    add click disable and color change to indv die view

--- BasicSwiftGame/InGameView/InGameSingleDiceView.swift
+++ BasicSwiftGame/InGameView/InGameSingleDiceView.swift
@@ -17,16 +17,20 @@ struct InGameSingleDiceView: View {
     var index: Int
     
     var body: some View {
-        Button(action: {
-            print("Make inactive")
-            print("\(self.currGame.DiceList[self.index].result)")
-        }) {
-            Image("dice-\(self.currGame.DiceList[self.index].result)")
-                .renderingMode(.original)
-                .resizable()
-                .scaledToFit()
-                .padding()
+        HStack {
+            Button(action: {
+                self.currGame.flag = 1
+                self.currGame.DiceList[self.index].isActive = false
+            }) {
+                Image("dice-\(self.currGame.DiceList[self.index].result)")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    
+            }
+            .disabled(!self.currGame.DiceList[self.index].isActive)
         }
+        .accentColor(.red)
     }
 }
 
