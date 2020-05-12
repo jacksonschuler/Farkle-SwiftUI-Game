0c431726d587972947176fcaba0bdd985f5a5f7d
BenBaker <bakerbenjamyn@gmail.com>
2020-05-11 20:41:08 -0600

    added styling to back buttons on rules and add player views

--- BasicSwiftGame/AddPlayerView/AddPlayerView.swift
+++ BasicSwiftGame/AddPlayerView/AddPlayerView.swift
@@ -13,6 +13,25 @@ import SwiftUI
 // players have been added players can
 // start the game
 struct AddPlayerView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var BackBtn: some View {
+        Button(action: {
+            self.presentationMode.wrappedValue.dismiss()
+        }) {
+            Text("Back")
+                .font(.body)
+                .padding(5)
+                .background(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255))
+                .cornerRadius(20)
+                .foregroundColor(Color(red: 221 / 255, green: 64 / 255, blue: 58 / 255))
+                .padding(5)
+                .overlay(
+                   RoundedRectangle(cornerRadius: 20)
+                       .stroke(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255), lineWidth: 5)
+                )
+        }
+    }
     
     var body: some View {
         VStack() {
@@ -33,6 +52,8 @@ struct AddPlayerView: View {
             Spacer()
                 
         }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: BackBtn)
         .background(Color(red: 62 / 255, green: 54 / 255, blue: 63 / 255).edgesIgnoringSafeArea(.vertical))
     }
 }
--- BasicSwiftGame/InGameView/InGameView.swift
+++ BasicSwiftGame/InGameView/InGameView.swift
@@ -21,6 +21,16 @@ struct InGameView: View {
                 self.showingAlert = true
             }) {
                 Text("End Game")
+                    .font(.body)
+                    .padding(5)
+                    .background(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255))
+                    .cornerRadius(20)
+                    .foregroundColor(Color(red: 221 / 255, green: 64 / 255, blue: 58 / 255))
+                    .padding(5)
+                    .overlay(
+                       RoundedRectangle(cornerRadius: 20)
+                           .stroke(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255), lineWidth: 5)
+                    )
             }
             .alert(isPresented: $showingAlert) {
                 Alert(title: Text("Game Over!"), message: currGame.find_winner(), dismissButton: Alert.Button.default(Text("End Game"), action:{
