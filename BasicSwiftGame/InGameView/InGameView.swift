d9f75c7e7d25ad7c75063fe2b63dfeaec05779c4
Robert Stefanyshin <robertstefanyshin@gmail.com>
2020-05-12 15:16:57 -0600

    Added scoreboard navlink to InGameView

--- BasicSwiftGame/InGameView/InGameView.swift
+++ BasicSwiftGame/InGameView/InGameView.swift
@@ -41,22 +41,21 @@ struct InGameView: View {
     }
     
     var ScoreBoardButton: some View {
-        Button(action: {
-//            ScoreBoardView()
-        }) {
+        
+        NavigationLink(destination: ScoreBoardView()) {
             Text("Score Board")
-            .font(.body)
-            .padding(5)
-            .background(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255))
-            .cornerRadius(20)
-            .foregroundColor(Color(red: 221 / 255, green: 64 / 255, blue: 58 / 255))
-            .padding(5)
+                .font(.body)
+                .padding(5)
+                .background(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255))
+                .cornerRadius(20)
+                .foregroundColor(Color(red: 221 / 255, green: 64 / 255, blue: 58 / 255))
+                .padding(5)
             .overlay(
-               RoundedRectangle(cornerRadius: 20)
-                   .stroke(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255), lineWidth: 5)
+                RoundedRectangle(cornerRadius: 20)
+                .stroke(Color(red: 255 / 255, green: 252 / 255, blue: 232 / 255), lineWidth: 5)
             )
-            }
         }
+    }
     
     var body: some View {
         InGameDiceView()
